0e0552e41c5e087f1ca95d1d57d15378375db750
Khadija-Craft <Khadija-Craft>
2022-08-04 19:59:46 +0400

    Tried adding controller Did not work

--- craftpms/craft_property_ms/doctype/building/building.js
+++ craftpms/craft_property_ms/doctype/building/building.js
@@ -1,34 +1,39 @@
+frappe.provide("erpnext")
 
-frappe.ui.form.on("Building", {
-    	onload: function(frm) {
+erpnext.PropertyController = frappe.ui.form.Controller.extend({
+	onload: function() {
+		frappe.call({
+				method:'craftpms.craft_property_ms.doctype.building.building.n_units',
+				args: {
+						'building_name': this.frm.doc.building_name,
+					  },
+		   callback: function(r) {
+			this.frm.set_value("no_of_units", r.message);
+			this.frm.save()
+		}
+			});
+			
 			frappe.call({
-        			method:'craftpms.craft_property_ms.doctype.building.building.n_units',
-        			args: {
-            				'building_name': frm.doc.building_name,
-      		        	},
-       		callback: function(r) {
-				frm.set_value("no_of_units", r.message);
-				frm.save()
-			}
-        		});
-        		
-        		frappe.call({
-        			method:'craftpms.craft_property_ms.doctype.building.building.available_units',
-        			args: {
-            				'building_name': frm.doc.building_name,
-      		        	},
-       		callback: function(r) {
-				frm.set_value("available_units", r.message);
-				frm.save()
-			}
-        		});
-        		
+				method:'craftpms.craft_property_ms.doctype.building.building.available_units',
+				args: {
+						'building_name': this.frm.doc.building_name,
+					  },
+		   callback: function(r) {
+			this.frm.set_value("available_units", r.message);
+			this.frm.save()
+		}
+			});
+			
   	},
-	refresh: function(frm) {
-		frm.add_custom_button(__('Unit'), function() {console.log("Running")}, __('Create'));
-		frm.page.set_inner_btn_group_as_primary(__('Create'));
-		// frm.add_custom_button(__('Unit'), function() {console.log("Running")});
+	refresh: function() {
+		this.frm.add_custom_button(__('Unit'), this.make_unit, __('Create'));
+		this.frm.page.set_inner_btn_group_as_primary(__('Create'));
+		// this.frm.add_custom_button(__('Unit'), function() {console.log("Running")});
+	},
+
+	make_unit: function() {
+		console.log("Running")
 	}
-});
-       
+})
+
 

a89e765e9ad047ff300a59d4d56631a3f2f156e0
riane <riane@craftinteractive.ae>
2022-08-05 12:53:45 +0400

    .

--- craftpms/craft_property_ms/doctype/unit/unit.py
+++ craftpms/craft_property_ms/doctype/unit/unit.py
@@ -1,6 +1,6 @@
 
 import datetime
-
+from datetime import datetime
 
 
 import frappe
@@ -11,25 +11,26 @@ class Unit(Document):
 		self.set_unit_status()
 	
 	def set_unit_status(self):
-
+		d = "2022-08-18"
+		d2 = datetime.strptime(d,"%Y-%m-%d").date()
 		if (self.contract_start_date and self.contract_end_date):
-			if datetime.date.today() < self.contract_start_date:
+			if d2 < self.contract_start_date:
 				self.unit_status = "Booked"
-			elif self.contract_start_date <= datetime.date.today() <= self.contract_end_date:
+			elif self.contract_start_date <= d2 <= self.contract_end_date:
 				self.unit_status = "On lease"
-			elif self.contract_end_date < datetime.date.today():
+			elif self.contract_end_date < d2:
 				self.unit_status = "Available"
 		else:
 			self.unit_status = "Available"
 		
 		
 def update_unit_status():
-	units = frappe.db.sql("""select name from `tabUnit`""")
+	units = frappe.db.sql("""select name from `tabUnit`""", as_dict= True)
 	for unit in units:
 		doc = frappe.get_doc("Unit", unit.name)
 		doc.set_unit_status()
-		unitstat = frappe.db.set_value("Unit", doc.name, "unit_status", doc.unit_status)
-	return unitstat
+		unitst = frappe.db.set_value("Unit", doc.name, "unit_status", doc.unit_status)
+	return unitst
 
 	
 
